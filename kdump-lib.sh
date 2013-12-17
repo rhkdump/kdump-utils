b0535afe2d9ec966536b6cb1010e0120f9ade039
WANG Chao <chaowang@redhat.com>
2013-12-17 14:54:15 +0800

    kdump-lib: add common variables and function for fence kdump
    Add following common variables and function:
    
    $FENCE_KDUMP_CONIFG: configuration file /etc/sysconfig/fence_kdump
    $FENCE_KDUMP_NODES: configuration file /etc/fence_kdump_nodes
    $FENCE_KDUMP_SEND: executable /usr/libexec/fence_kdump_send
    is_fence_kdump(): used to determine if the system is in a cluster and
    configured with fence_kdump.
    
    Signed-off-by: WANG Chao <chaowang@redhat.com>
    Tested-by: Zhi Zou <zzou@redhat.com>
    Tested-by: Marek Grac <mgrac@redhat.com>
    Acked-by: Vivek Goyal <vgoyal@redhat.com>

--- kdump-lib.sh
+++ kdump-lib.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 #
-# Kdump common functions
+# Kdump common variables and functions
 #
+
+FENCE_KDUMP_CONFIG="/etc/sysconfig/fence_kdump"
+FENCE_KDUMP_SEND="/usr/libexec/fence_kdump_send"
+FENCE_KDUMP_NODES="/etc/fence_kdump_nodes"
 
 is_ssh_dump_target()
 {
@@ -21,4 +25,15 @@ is_raw_dump_target()
 strip_comments()
 {
     echo $@ | sed -e 's/\(.*\)#.*/\1/'
+}
+
+# Check if fence kdump is configured in cluster
+is_fence_kdump()
+{
+    # no pcs or fence_kdump_send executables installed?
+    type -P pcs > /dev/null || return 1
+    [ -x $FENCE_KDUMP_SEND ] || return 1
+
+    # fence kdump not configured?
+    (pcs cluster cib | grep -q 'type="fence_kdump"') &> /dev/null || return 1
 }
