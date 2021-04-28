rhkdump/kdump-utils

commit e8ef4db8ff91c57d499677873762c3524e786aa5

Fix dump_fs mount point detection and fallback mount
Simplify the code and fix mount point detection. The code logic is now
much simpler: if $1 is not a mount point, call "mount --target $1" again
to try mount it. "mount --target" cmd itself can handle all the /etc/fstab
parsing job, so drop the buggy and complex bash code.

Signed-off-by: Kairui Song <kasong@redhat.com>
Acked-by: Pingfan Liu <piliu@redhat.com>

kdump-lib-initramfs.sh
@@ -117,26 +117,15 @@ dump_fs()
 {
     local _exitcode
     local _mp=$1
-    local _dev=$(get_mount_info SOURCE target $_mp -f)
-    local _op=$(get_mount_info OPTIONS target $_mp -f)
+    ddebug "dump_fs _mp=$_mp"
 
-    ddebug "_mp=$_mp _dev=$_dev _op=$_op"
-
-    # If dump path have a corresponding device entry but not mounted, mount it.
-    if [ -n "$_dev" ] && [ "$_dev" != "rootfs" ]; then
-        if ! is_mounted "$_mp"; then
-            dinfo "dump target $_dev is not mounted, trying to mount..."
-            mkdir -p $_mp
-            mount -o $_op $_dev $_mp
-
-            if [ $? -ne 0 ]; then
-                derror "mounting failed (mount point: $_mp, option: $_op)"
-                return 1
-            fi
+    if ! is_mounted "$_mp"; then
+        dinfo "dump path \"$_mp\" is not mounted, trying to mount..."
+        mount --target $_mp
+        if [ $? -ne 0 ]; then
+            derror "failed to dump to \"$_mp\", it's not a mount point!"
+            return 1
         fi
-    else
-        derror "failed to dump to \"$_mp\", it's not a mount point!"
-        return 1
     fi
 
     # Remove -F in makedumpfile case. We don't want a flat format dump here.

commit d0e9c51e0de57273a99af8484515f213b8414e65

fadump: fix dump capture failure to root disk
If the dump target is the root disk, kdump scripts add an entry in
/etc/fstab for root disk with /sysroot as the mount point. The root
disk, passed through root=<> kernel commandline parameter, is mounted
at /sysroot in read-only mode before switching from initial ramdisk.
So, in fadump mode, a remount of /sysroot to read-write mode is needed
to capture dump successfully, because /sysroot is already mounted as
read-only based on root=<> boot parameter.

Commit e8ef4db8ff91 ("Fix dump_fs mount point detection and fallback
mount") removed initialization of $_op variable, the variable holding
the options the dump target was mounted with, leading to the below
error as remount was skipped:

  kdump[586]: saving to /sysroot/var/crash/127.0.0.1-2021-04-22-07:22:08/
  kdump.sh[587]: mkdir: cannot create directory '/sysroot/var/crash/127.0.0.1-2021-04-22-07:22:08/': Read-only file system
  kdump[589]: saving vmcore failed

Restore $_op variable initialization in dump_fs() function to fix this.

Fixes: e8ef4db8ff91 ("Fix dump_fs mount point detection and fallback mount")
Signed-off-by: Hari Bathini <hbathini@linux.ibm.com>
Acked-by: Kairui Song <kasong@redhat.com>

kdump-lib-initramfs.sh
@@ -119,7 +119,8 @@ dump_fs()
 {
     local _exitcode
     local _mp=$1
-    ddebug "dump_fs _mp=$_mp"
+    local _op=$(get_mount_info OPTIONS target $_mp -f)
+    ddebug "dump_fs _mp=$_mp _opts=$_op"
 
     if ! is_mounted "$_mp"; then
         dinfo "dump path \"$_mp\" is not mounted, trying to mount..."
@@ -139,8 +140,8 @@ dump_fs()
 
     # Only remount to read-write mode if the dump target is mounted read-only.
     if [[ "$_op" = "ro"* ]]; then
-       dinfo "Mounting Dump target $_dev in rw mode."
-       mount -o remount,rw $_dev $_mp || return 1
+       dinfo "Remounting the dump target in rw mode."
+       mount -o remount,rw $_mp || return 1
     fi
 
     mkdir -p $_dump_path || return 1