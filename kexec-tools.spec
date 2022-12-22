rhkdump/kdump-utils

commit e218128e282066440a4a654707309d785066d79a

Only try to reset crashkernel for osbuild during package install
Resolves: https://bugzilla.redhat.com/show_bug.cgi?id=2060319

Currently, kexec-tools tries to reset crashkernel when using anaconda to
install the system. But grubby isn't ready and complains that,
  10:33:17,631 INF packaging: Configuring (running scriptlet for): kernel-core-5.14.0-70.el9.x86_64 1645746534 03dcd32db234b72440ee6764d59b32347c5f0cd98ac3fb55beb47214a76f33b4
  10:34:16,696 INF dnf.rpm: grep: /boot/grub2/grubenv: No such file or directory
  grep: /boot/grub2/grubenv: No such file or directory

We only need to try resetting crashkernel for osbuild. Skip it for other
cases. To tell if it's package install instead of package upgrade, make
use of %pre to write a file /tmp/kexec-tools-install when "$1 == 1" [1].

[1] https://docs.fedoraproject.org/en-US/packaging-guidelines/Scriptlets/#_syntax

Reported-by: Jan Stodola <jstodola@redhat.com>
Signed-off-by: Coiby Xu <coxu@redhat.com>
Reviewed-by: Lichen Liu <lichenliu@redhat.com>

kdumpctl
@@ -1597,6 +1597,12 @@ reset_crashkernel()
 	fi
 }
 
+# to tell if it's package install other than upgrade
+_is_package_install()
+{
+	[[ -f /tmp/kexec_tools_package_install ]]
+}
+
 # update the crashkernel value in GRUB_ETC_DEFAULT if necessary
 #
 # called by reset_crashkernel_after_update and inherit its array variable
@@ -1605,6 +1611,10 @@ update_crashkernel_in_grub_etc_default_after_update()
 {
 	local _crashkernel _fadump_val
 	local _dump_mode _old_default_crashkernel _new_default_crashkernel
+
+	if _is_package_install; then
+		return
+	fi
 
 	_crashkernel=$(_read_kernel_arg_in_grub_etc_default crashkernel)
 
@@ -1680,6 +1690,12 @@ reset_crashkernel_for_installed_kernel()
 {
 	local _installed_kernel _running_kernel _crashkernel _crashkernel_running
 	local _dump_mode_running _fadump_val_running
+
+	# During package install, only try to reset crashkernel for osbuild
+	# thus to avoid calling grubby when installing os via anaconda
+	if _is_package_install && ! _is_osbuild; then
+		return
+	fi
 
 	if ! _installed_kernel=$(_find_kernel_path_by_release "$1"); then
 		exit 1

kexec-tools.spec
@@ -265,6 +265,11 @@ if [ ! -f /run/ostree-booted ] && [ $1 == 2 ] && grep -q get-default-crashkernel
   kdumpctl get-default-crashkernel fadump > /tmp/old_default_crashkernel_fadump 2>/dev/null
 %endif
 fi
+# indicate it's package install so kdumpctl later will only reset crashkernel
+# value for osbuild.
+if [ $1 == 1 ]; then
+  touch /tmp/kexec_tools_package_install
+fi
 # don't block package update
 :
 

commit 5951b5e26823b6bedf3237bd169a781b03f25031

Don't try to update crashkernel when bootloader is not installed
Currently when using anaconda to install the OS, the following errors
occur,

    INF packaging: Configuring (running scriptlet for): kernel-core-5.14.0-70.el9.x86_64 ...
    INF dnf.rpm: grep: /boot/grub2/grubenv: No such file or directory
    grep: /boot/grub2/grubenv: No such file or directory
    grep: /boot/grub2/grubenv: No such file or directory
    grep: /boot/grub2/grubenv: No such file or directory
    ...
    INF packaging: Configuring (running scriptlet for): kexec-tools-2.0.23-9.el9.x86_64 ...
    INF dnf.rpm: grep: /boot/grub2/grubenv: No such file or directory
    grep: /boot/grub2/grubenv: No such file or directory
    grep: /boot/grub2/grubenv: No such file or directory

Or for s390, the following errors occur,

    INF packaging: Configuring (running scriptlet for): kernel-core-5.14.0-71.el9.s390x ...
    03:37:51,232 INF dnf.rpm: grep: /etc/zipl.conf: No such file or directory
    grep: /etc/zipl.conf: No such file or directory
    grep: /etc/zipl.conf: No such file or directory

    INF packaging: Configuring (running scriptlet for): kexec-tools-2.0.23-9_1.el9_0.s390x ...
    INF dnf.rpm: grep: /etc/zipl.conf: No such file or directory

This is because when anaconda installs the packages, bootloader hasn't
been installed and /boot/grub2/grubenv or /etc/zipl.conf doesn't exist.
So don't try to update crashkernel when bootloader isn't ready to avoid
the above errors.

Note this is the second attempt to fix this issue. Previously a file
/tmp/kexec_tools_package_install was created to avoid running the
related code thus to avoid the above errors but unfortunately that
approach has two issues a) somehow osbuild doesn't delete it for RHEL b)
this file could still exist if users manually remove kexec-tools.

Fixes: e218128 ("Only try to reset crashkernel for osbuild during package install")
Reported-by: Jan Stodola <jstodola@redhat.com>
Signed-off-by: Coiby Xu <coxu@redhat.com>

kdumpctl
@@ -1611,10 +1611,13 @@ reset_crashkernel()
 	fi
 }
 
-# to tell if it's package install other than upgrade
-_is_package_install()
+_is_bootloader_installed()
 {
-	[[ -f /tmp/kexec_tools_package_install ]]
+	if [[ $(uname -r) == s390x ]]; then
+		test -f /etc/zipl.conf
+	else
+		test -f /boot/grub2/grub.cfg
+	fi
 }
 
 # update the crashkernel value in GRUB_ETC_DEFAULT if necessary
@@ -1625,10 +1628,6 @@ update_crashkernel_in_grub_etc_default_after_update()
 {
 	local _crashkernel _fadump_val
 	local _dump_mode _old_default_crashkernel _new_default_crashkernel
-
-	if _is_package_install; then
-		return
-	fi
 
 	if [[ $(uname -m) == s390x ]]; then
 		return
@@ -1659,7 +1658,7 @@ reset_crashkernel_after_update()
 	local _kernel _crashkernel _dump_mode _fadump_val _old_default_crashkernel _new_default_crashkernel
 	declare -A _crashkernel_vals
 
-	if _is_package_install; then
+	if ! _is_bootloader_installed; then
 		return
 	fi
 
@@ -1715,7 +1714,7 @@ reset_crashkernel_for_installed_kernel()
 
 	# During package install, only try to reset crashkernel for osbuild
 	# thus to avoid calling grubby when installing os via anaconda
-	if _is_package_install && ! _is_osbuild; then
+	if ! _is_bootloader_installed && ! _is_osbuild; then
 		return
 	fi
 

kexec-tools.spec
@@ -268,11 +268,6 @@ if [ ! -f /run/ostree-booted ] && [ $1 == 2 ] && grep -q get-default-crashkernel
   kdumpctl get-default-crashkernel fadump > /tmp/old_default_crashkernel_fadump 2>/dev/null
 %endif
 fi
-# indicate it's package install so kdumpctl later will only reset crashkernel
-# value for osbuild.
-if [ $1 == 1 ]; then
-  touch /tmp/kexec_tools_package_install
-fi
 # don't block package update
 :
 