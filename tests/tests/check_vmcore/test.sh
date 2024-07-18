7428f4e1ff2634d56c0f0c0165f5138904d3d68a
Coiby Xu <coxu@redhat.com>
2024-07-18 11:02:23 +0800

    tests: add Test Management Tool kdump NFS tests
    Currently, all integration tests fail F40 because of failing to install
    package and enable services on the cloud image properly. TMT (Test Management
    Tool) supports running tests on virtual machines and provides an
    user-friendly interface (YAML) to define tasks like installing packages,
    running commands on the virtual machines. By using TMT, we no longer
    need to spend time and efforts on maintaining the current test framework
    and can also enjoy rich handy features like managing tests provided by TMT.
    
    There are six test steps in TMT. Quoting [1], here is an overview,
     - discover
       Gather information about test cases to be executed.
    
     - provision
       Provision an environment for testing or use localhost.
    
     - prepare
       Prepare the environment for testing.
    
     - execute
       Run tests using the specified executor.
    
     - report
       Provide test results overview and send reports.
    
     - finish
       Perform the finishing tasks and clean up provisioned guests.
    
    And each step step is configurable via the so-called plans [2]. TMT also
    uses plans to group relevant tests and select needed tests.
    
    This commit adds the test of sending crash dump to a NFS target by
    setting two VMs using TMT's MultiHost Testing feature [3],
     - client
       Setup up NFS dump target and trigger crash
     - server
       acts as an NFS server and check sent vmcore
    
    The scaffold of TMT test project can be created as follows,
    
        mkdir tests && cd tests
        tmt init --template mini
        mv plans/example/{example,kdump}.fmf
    
        tmt tests create /setup/default_crashkernel -t beakerlib
        tmt tests create /setup/nfs_server -t beakerlib
        tmt tests create /test/trigger_crash -t beakerlib
        tmt tests create /test/check_vmcore -t beakerlib
    
    Then we fill in the content for created plans and tests,
    
        # tmt run -a
        /var/tmp/tmt/run-017
    
        /plans/kdump
            discover
                how: fmf
                name: client-setup
                directory: /root/kdump-tests
                tests: /setup/kdump
                how: fmf
                name: server-setup
        ...
                execute task #4: server-test on server
                how: tmt
    
                summary: 4 tests executed
            report
                how: display
                summary: 4 tests passed
            finish
    
    A main.fmf is put under the root directory of tests and setup for other
    tests to inherit [4].
    
    When there is a kernel panic, ssh access to the guest will exit with
    with 255 and TMT will retreat it as an error. We can use
    restart-on-exit-code=255 [5] to let TMT this is expected.
    
    [1] https://tmt.readthedocs.io/en/stable/overview.html#run
    [2] https://tmt.readthedocs.io/en/stable/spec/plans.html
    [3] https://tmt.readthedocs.io/en/stable/guide.html#multihost-testing
    [4] https://tmt.readthedocs.io/en/stable/guide.html#inheritance
    [5] https://tmt.readthedocs.io/en/latest/spec/tests.html#restart
    
    Signed-off-by: Coiby Xu <coxu@redhat.com>

---
+++ tests/tests/check_vmcore/test.sh
@@ -0,0 +1,56 @@
+#!/bin/bash -eux
+
+has_valid_vmcore_dir() {
+    local path=$1
+    local vmcore_dir
+    local vmcore="<invalid>"
+
+    MAX_WAIT_TIME=300
+    _waited=0
+    while [ $_waited -le $MAX_WAIT_TIME ]; do
+        if vmcore_dir=$(find "$path/"* -maxdepth 0 2> /dev/null); then
+            break
+        fi
+        sleep 1
+        _waited=$((_waited + 1))
+    done
+
+    if [[ $_waited -ge $MAX_WAIT_TIME ]]; then
+        echo "No Vmcore found!" 1>&2
+        return 1
+    fi
+
+    echo "Found a vmcore dir \"$vmcore_dir\":"
+    # Checking with `crash` is slow and consume a lot of memory/disk,
+    # just do a sanity check by check if log are available.
+    if [ -e "$vmcore_dir/vmcore" ]; then
+        vmcore=$vmcore_dir/vmcore
+        makedumpfile --dump-dmesg "$vmcore" "$vmcore_dir/vmcore-dmesg.txt.2" || {
+            echo "Failed to retrive dmesg from vmcore!"
+            return 1
+        }
+    elif [ -e "$vmcore_dir/vmcore.flat" ]; then
+        vmcore=$vmcore_dir/vmcore.flat
+        makedumpfile -R "$vmcore_dir/vmcore" < "$vmcore" || return 1
+        makedumpfile --dump-dmesg "$vmcore_dir/vmcore" "$vmcore_dir/vmcore-dmesg.txt.2" || {
+            echo "Failed to retrive dmesg from vmcore!"
+            return 1
+        }
+        rm "$vmcore_dir/vmcore"
+    else
+        echo "The vmcore dir is empty!"
+        return 1
+    fi
+
+    if ! diff -w $vmcore_dir/vmcore-dmesg.txt.2 $vmcore_dir/vmcore-dmesg.txt; then
+        echo "Dmesg retrived from vmcore is different from dump version!"
+        return 1
+    fi
+
+    echo "VMCORE: $vmcore"
+    echo "KERNEL VERSION: $(rpm -q kernel-core)"
+
+    return 0
+}
+
+has_valid_vmcore_dir "/var/tmp/nfsshare/var/crash/"
