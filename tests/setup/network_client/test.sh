100b9ea3e8cc968406048654ae5a7cc635d6e7a6
Coiby Xu <coxu@redhat.com>
2024-07-30 09:43:33 +0800

    tests: add SSH dumping test plan
    TMT stores server SSH key in
    /var/tmp/tmt/run-*/plans/ssh/provision/server/id_ecdsa.
    
    Signed-off-by: Coiby Xu <coxu@redhat.com>

--- tests/setup/network_client/test.sh
+++ tests/setup/network_client/test.sh
@@ -50,7 +50,21 @@ rlLog "This system is: $(hostname) ${MY_IP}"
 rlPhaseEnd
 
 rlPhaseStartTest
-rlRun "echo nfs $SERVER:/var/tmp/nfsshare > /etc/kdump.conf"
+if [[ $REMOTE_TYPE == NFS ]]; then
+    rlRun "echo nfs $SERVER:/var/tmp/nfsshare > /etc/kdump.conf"
+elif [[ $REMOTE_TYPE == SSH ]]; then
+    TMT_TEST_PLAN_ROOT=${TMT_PLAN_DATA%data}
+    SERVER_SSH_KEY=${TMT_TEST_PLAN_ROOT}/provision/server/id_ecdsa
+    if test -f "$SERVER_SSH_KEY"; then
+        rlRun "ssh-keyscan -H $SERVER > /root/.ssh/known_hosts"
+        rlRun "ssh root@$SERVER -i $SERVER_SSH_KEY 'mkdir /var/crash'"
+        rlRun "echo ssh root@$SERVER > /etc/kdump.conf"
+        rlRun "echo sshkey $SERVER_SSH_KEY >> /etc/kdump.conf"
+        rlRun "echo core_collector makedumpfile -l --message-level 7 -d 31 -F >> /etc/kdump.conf"
+    else
+        rlDie "Server SSH Key not found, something wrong"
+    fi
+fi
 rlPhaseEnd
 
 rlJournalEnd
