200107e45ac2619bf7d865a19ff896a83b97fe56
Coiby Xu <coxu@redhat.com>
2024-12-10 16:38:23 +0800

    Note user-specified crashkernel value will be overwritten by default value
    Resolves: https://issues.redhat.com/browse/RHEL-69568
    
    When kdump-utils gets updated automatically i.e. not updated manually by
    user, users won't notice custom crashkernel value gets overwritten as
    the logs in /var/log/dnf.rpm.log may be ignored,
        2024-12-10T02:57:12-0500 INFO kdump: For kernel=/boot/vmlinuz-6.11.0-28.el10.x86_64, crashkernel=1G-4G:192M,4G-64G:256M,64G-:512M now. Please reboot the system for the change to take effect. Note if you don't want kexec-tools to manage the crashkernel kernel parameter, please set auto_reset_crashkernel=no in /etc/kdump.conf.
    
    So add a note to kdump.conf.
    
    Suggested-by: Ryosuke Yasuoka <ryasuoka@redhat.com>
    
    Signed-off-by: Coiby Xu <coxu@redhat.com>

--- gen-kdump-conf.sh
+++ gen-kdump-conf.sh
@@ -23,7 +23,8 @@ generate()
 #           - whether to reset kernel crashkernel to new default value
 #             or not when kexec-tools updates the default crashkernel value and
 #             existing kernels using the old default kernel crashkernel value.
-#             The default value is yes.
+#             The default value is yes. Note the user-specified value will be
+#             overwritten to the default crahskernel value.
 #
 # raw <partition>
 #           - Will dd /proc/vmcore into <partition>.
