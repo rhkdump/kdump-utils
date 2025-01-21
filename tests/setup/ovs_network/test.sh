d5ddc8508b56d9c48f59a91b09880ba78d37f412
Coiby Xu <coxu@redhat.com>
2025-01-21 11:46:06 +0800

    tests: add NFS over OVS bridge network
    This test plan is the same to plans/nfs.fmf except for an extra step to
    set up OVS bridge network.
    
    Signed-off-by: Coiby Xu <coxu@redhat.com>

---
+++ tests/setup/ovs_network/test.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -eux
+
+ovs_configure_script=/usr/local/bin/configure-ovs.sh
+
+extract_configure-ovs-script() {
+   local _script_url
+
+   _script_url=https://github.com/openshift/machine-config-operator/raw/refs/heads/master/templates/common/_base/files/configure-ovs-network.yaml
+
+   if curl -sL $_script_url | grep -A2000 '#!/bin/bash' > "$ovs_configure_script";  then
+       chmod +x "$ovs_configure_script"
+   else
+       return 1
+   fi
+}
+
+create_bond_network() {
+    local _ifname
+
+    _ifname=$(ip route show default | awk '{print $5}') || return 1
+    nmcli con add type bond ifname bond0
+    nmcli con add type ethernet ifname "$_ifname" master bond0
+    nmcli con up "bond-slave-$_ifname"
+}
+
+
+systemctl enable --now openvswitch
+# restart NM so the ovs plugin can be activated
+systemctl restart NetworkManager
+
+mkdir -p /etc/ovnk
+echo bond0 > /etc/ovnk/iface_default_hint
+
+extract_configure-ovs-script
+create_bond_network
+"$ovs_configure_script" OVNKubernetes
+
+# make the connections persistent
+cp /run/NetworkManager/system-connections/* /etc/NetworkManager/system-connections/
