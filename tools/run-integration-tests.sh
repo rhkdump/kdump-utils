414fd847849257efabc70eead0c666dbd094b813
Coiby Xu <coxu@redhat.com>
2025-11-27 16:16:57 +0800

    tests: Enable local and nfs test plans for CoreOS
    This can help us know if kdump is stable enough for CoreOS and catch any
    problem early.
    
    Signed-off-by: Coiby Xu <coxu@redhat.com>

--- tools/run-integration-tests.sh
+++ tools/run-integration-tests.sh
@@ -29,12 +29,25 @@ if [[ ! -f $rpm_path ]]; then
 	echo "Failed to find built kdump-utils rpm ($rpm_path doesn't eixst)"
 fi
 
-cd tests && tmt --context distro="fedora-${fedora_version}" run --environment CUSTOM_MIRROR="$mirror" --environment KDUMP_UTILS_RPM="$rpm_path" -a provision -h virtual -i fedora:"$fedora_version" plans --name lvm2_thinp
+tmt_run_env=(--environment CUSTOM_MIRROR="$mirror" --environment KDUMP_UTILS_RPM="$rpm_path" --environment RESOURCE_URL=https://gitlab.cee.redhat.com/kernel-qe/kernel/-/raw/master/kdump/internal/internal_resources.sh --environment AUTO_CONFIG=pek)
+
+tmt_context=(--context distro="fedora-${fedora_version}")
+cd tests && tmt "${tmt_context[@]}" run "${tmt_run_env[@]}" -a provision -h virtual -i fedora:"$fedora_version" plans --name lvm2_thinp
 
 if [[ $fedora_version == rawhide ]]; then
 	cd ../kernel-tests-plans
-	tmt --context distro="fedora-${fedora_version}" --context install_built_rpm=yes run \
-		--environment RESOURCE_URL=https://gitlab.cee.redhat.com/kernel-qe/kernel/-/raw/master/kdump/internal/internal_resources.sh \
-		--environment AUTO_CONFIG=pek --environment CUSTOM_MIRROR="$mirror" --environment KDUMP_UTILS_RPM="$rpm_path" -a \
-		provision -h virtual -c system -i fedora:"$fedora_version" plans --name nfs_ovs
+	tmt_context+=(--context install_built_rpm=yes)
+	tmt "${tmt_context[@]}" run \
+		"${tmt_run_env[@]}" \
+		-a provision -h virtual -c system -i fedora:"$fedora_version" plans --name nfs_ovs
+
+	# Run test plans for CoreOS
+	for _stream in stable testing next; do
+		core_os_stream="fedora-coreos-${_stream}"
+		tmt_context[1]="distro=${core_os_stream}"
+		tmt_context+=(--context variant=CoreOS)
+		tmt "${tmt_context[@]}" run \
+			"${tmt_run_env[@]}" \
+			-a provision -h virtual -c system -i "${core_os_stream}" plans --name "local|nfs$"
+	done
 fi
