ede0a68c62154a47e70395339f317ec2f48b1521
Coiby Xu <coxu@redhat.com>
2026-01-09 16:27:38 +0800

    Standalone tools/build-rpm.sh
    Extract the code of building RPM from tools/run-integration-tests.sh as
    tools/build-rpm.sh so it can be re-used by developers.
    
    Suggested-by: Sourabh Jain <sourabhjain@linux.ibm.com>
    Signed-off-by: Coiby Xu <coxu@redhat.com>

--- tools/run-integration-tests.sh
+++ tools/run-integration-tests.sh
@@ -8,25 +8,9 @@ fedora_version=${1:-40}
 mirror=${2:-https://mirrors.tuna.tsinghua.edu.cn/fedora}
 [[ $fedora_version == rawhide ]] && mirror=https://mirrors.sjtug.sjtu.edu.cn/fedora/linux
 
-dist_abbr=.fc$fedora_version
-
-VERSION=$(rpmspec -q --queryformat "%{VERSION}" kdump-utils.spec)
-SRC_ARCHIVE=kdump-utils-$VERSION.tar.gz
-if ! git archive --format=tar.gz -o "$SRC_ARCHIVE" --prefix="kdump-utils-$VERSION/" HEAD; then
-	echo "Failed to create kdump-utils source archive"
-	exit 1
-fi
-
-if ! rpmbuild -ba -D "dist $dist_abbr" -D "_sourcedir $(pwd)" -D "_builddir $(pwd)" -D "_srcrpmdir $(pwd)" -D "_rpmdir $(pwd)" kdump-utils.spec; then
+if ! rpm_path=$(./tools/build-rpm.sh "$fedora_version"); then
 	echo "Failed to build kdump-utils rpm"
 	exit 1
-fi
-
-arch=$(uname -m)
-rpm_name=$(rpmspec -D "dist $dist_abbr" -q --queryformat '%{NAME}-%{VERSION}-%{RELEASE}.%{ARCH}' kdump-utils.spec)
-rpm_path="$(pwd)/${arch}/${rpm_name}.rpm"
-if [[ ! -f $rpm_path ]]; then
-	echo "Failed to find built kdump-utils rpm ($rpm_path doesn't eixst)"
 fi
 
 tmt_run_env=(--environment CUSTOM_MIRROR="$mirror" --environment KDUMP_UTILS_RPM="$rpm_path" --environment RESOURCE_URL=https://gitlab.cee.redhat.com/kernel-qe/kernel/-/raw/master/kdump/internal/internal_resources.sh --environment AUTO_CONFIG=pek)
