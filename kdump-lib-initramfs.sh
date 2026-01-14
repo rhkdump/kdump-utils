rhkdump/kdump-utils

commit d81109cf9291250b42434fc51798c93b613c4ee2

kdump-lib-initramfs: rewrite kdump_get_conf_val
Previously, kdump_get_conf_val() relied on a "magic" regular
expression to parse kdump.conf, making maintenance and
extension difficult. This patch rewrites the parsing logic
to be more human-readable.

The '\|' needs to be changed to a single '|' because after
the rewrite we will use another tool to parse the options to
find.

Furthermore, kdump_read_conf() has been refactored to invoke
kdump_get_conf_val(), as their logic is functionally identical.

Signed-off-by: Philipp Rudo <prudo@redhat.com>
Signed-off-by: Lichen Liu <lichliu@redhat.com>

kdump-lib-initramfs.sh
@@ -19,19 +19,52 @@ LUKS_KEY_PRFIX="kdump-cryptsetup:vk-"
 # Read kdump config in well formated style
 kdump_read_conf()
 {
-	# Following steps are applied in order: strip trailing comment, strip trailing space,
-	# strip heading space, match non-empty line, remove duplicated spaces between conf name and value
-	[ -f "$KDUMP_CONFIG_FILE" ] && sed -n -e "s/#.*//;s/\s*$//;s/^\s*//;s/\(\S\+\)\s*\(.*\)/\1 \2/p" $KDUMP_CONFIG_FILE
+	kdump_get_conf_val ""
 }
 
 # Retrieves config value defined in kdump.conf
-# $1: config name, sed regexp compatible
+# $1: config name, if empty print full config
 kdump_get_conf_val()
 {
-	# For lines matching "^\s*$1\s+", remove matched part (config name including space),
-	# remove tailing comment, space, then store in hold space. Print out the hold buffer on last line.
-	[ -f "$KDUMP_CONFIG_FILE" ] &&
-		sed -n -e "/^\s*\($1\)\s\+/{s/^\s*\($1\)\s\+//;s/#.*//;s/\s*$//;h};\${x;p}" $KDUMP_CONFIG_FILE
+	_to_find="$1"
+	_found=""
+
+	[ -f "$KDUMP_CONFIG_FILE" ] || return
+	while read -r _line; do
+		_line="$(echo "$_line" | tr -s "[:blank:]" " ")"
+		case "$_line" in
+		"" | \#*)
+			continue
+			;;
+		*\#*)
+			_line="${_line%%\#*}"
+			_line="${_line% }"
+			;;
+		esac
+
+		_opt=${_line%% *}
+		_val=${_line#* }
+
+		case "$_val" in
+		\"*\")
+			# Remove quotes
+			_val="${_val#\"}"
+			_val="${_val%\"}"
+			;;
+		esac
+
+		if [ -z "$_to_find" ]; then
+			echo "$_opt $_val"
+		elif echo "$_opt" | grep -q -E "^($_to_find)$"; then
+			# make sure to only return the last match to mirror the
+			# old behavior
+			_found="$_val"
+		fi
+	done < "$KDUMP_CONFIG_FILE"
+	[ -n "$_found" ] && echo "$_found"
+
+	# make sure we return 0 even when a option isn't set
+	return 0
 }
 
 is_mounted()

kdump-lib.sh
@@ -87,7 +87,7 @@ to_dev_name()
 
 is_user_configured_dump_target()
 {
-	[[ $(kdump_get_conf_val "ext[234]\|xfs\|btrfs\|minix\|raw\|nfs\|ssh\|virtiofs") ]] || is_mount_in_dracut_args
+	[[ $(kdump_get_conf_val "ext[234]|xfs|btrfs|minix|raw|nfs|ssh|virtiofs") ]] || is_mount_in_dracut_args
 }
 
 get_block_dump_target()
@@ -98,7 +98,7 @@ get_block_dump_target()
 		return
 	fi
 
-	_target=$(kdump_get_conf_val "ext[234]\|xfs\|btrfs\|minix\|raw\|virtiofs")
+	_target=$(kdump_get_conf_val "ext[234]|xfs|btrfs|minix|raw|virtiofs")
 	[[ -n $_target ]] && to_dev_name "$_target" && return
 
 	_target=$(get_dracut_args_target "$(kdump_get_conf_val "dracut_args")")
@@ -116,7 +116,7 @@ get_block_dump_target()
 
 is_dump_to_rootfs()
 {
-	[[ $(kdump_get_conf_val 'failure_action\|default') == dump_to_rootfs ]]
+	[[ $(kdump_get_conf_val 'failure_action|default') == dump_to_rootfs ]]
 }
 
 is_lvm2_thinp_dump_target()

commit 638c48ae3e6b2682222240b60c713def970bd0df

kdump-lib-initramfs: Fix performance regression in kdump_get_conf_val
Rewriting kdump_get_conf_val in Bash lead to a massive performance
regression. On my test system starting the kdump service took

$ time kdumpctl start
real    0m13.134s
user    0m8.828s
sys     0m7.450s

which is ~20 times slower compared to kdump-utils-1.0.59-1.fc44 with

$ time kdumpctl start
real    0m0.641s
user    0m0.208s
sys     0m0.538s

Looking at the traces shows that this is caused because Bash now has to
handle the whole kdump.conf, including the extensive comment at the
start, every time kdump_get_conf_val is called. This is done multiple
times when starting the kdump service and is often cloaked by other
functions, e.g. is_ssh_dump_target() or get_save_path().

To fix the issue remove comments and empty lines in a regex again so
that the Bash code only has to handle valid config entries. With this
change alone the performance is almost as good as the original version
with

$ time kdumpctl start
real    0m0.780s
user    0m0.330s
sys     0m0.604s

In the long run it would make sense to also reduce the number of calls
to kdump_get_conf_val.

This patch also fixes the issue that subsequent blanks are replaced by a
single space. Usually this is not an issue but there are corner cases,
e.g. in printf-like format strings passed as an argument, where the new
behaviour is undesirable.

Fixes: d81109c ("kdump-lib-initramfs: rewrite kdump_get_conf_val")
Signed-off-by: Philipp Rudo <prudo@redhat.com>

kdump-lib-initramfs.sh
@@ -30,38 +30,40 @@ kdump_get_conf_val()
 	_found=""
 
 	[ -f "$KDUMP_CONFIG_FILE" ] || return
-	while read -r _line; do
-		_line="$(echo "$_line" | tr -s "[:blank:]" " ")"
-		case "$_line" in
-		"" | \#*)
-			continue
-			;;
-		*\#*)
-			_line="${_line%%\#*}"
-			_line="${_line% }"
-			;;
-		esac
 
-		_opt=${_line%% *}
-		_val=${_line#* }
+	# On lines that are _not_ comments or empty remove...
+	# Note: The additional braces {} are required as piping into a while
+	# loop creates a sub-shell. So without the braces $_found would only be
+	# set inside the loop but empty outside of it.
+	grep -Ev -e '^\s*#' -e '^\s*$' "$KDUMP_CONFIG_FILE" | {
+		while read -r _opt _val; do
+			# ...trailing comments...
+			case "$_val" in
+			*\#*)
+				_val="${_val%%#*}"
+				_val="${_val%"${_val##*[![:space:]]}"}"
+				;;
+			esac
 
-		case "$_val" in
-		\"*\")
-			# Remove quotes
-			_val="${_val#\"}"
-			_val="${_val%\"}"
-			;;
-		esac
+			# ...quotes
+			case "$_val" in
+			\"*\")
+				_val="${_val#\"}"
+				_val="${_val%\"}"
+				;;
+			esac
 
-		if [ -z "$_to_find" ]; then
-			echo "$_opt $_val"
-		elif echo "$_opt" | grep -q -E "^($_to_find)$"; then
-			# make sure to only return the last match to mirror the
-			# old behavior
-			_found="$_val"
-		fi
-	done < "$KDUMP_CONFIG_FILE"
-	[ -n "$_found" ] && echo "$_found"
+			if [ -z "$_to_find" ]; then
+				echo "$_opt $_val"
+			elif echo "$_opt" | grep -q -E "^($_to_find)$"; then
+				# make sure to only return the last match to
+				# mirror the old behavior
+				_found="$_val"
+			fi
+		done
+
+		[ -n "$_found" ] && echo "$_found"
+	}
 
 	# make sure we return 0 even when a option isn't set
 	return 0