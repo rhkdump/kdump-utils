4c29cff512e4f3255b965bcb522f4315fe67e293
Tao Liu <ltao@redhat.com>
2026-07-17 15:38:15 +1200

    tests: Restart the nfs-server service after "systemctl enable --now"
    A nfs mount hang is observed during nfs testing in the client side:
    
      [root@client ~]# mount 192.168.122.17:/var/tmp/nfsshare /mnt
      <<------- hang
    
    After restarting the nfs-server service in the server side, the nfs client
    mount can exit successfully in a short time:
    
      [root@server ~]# systemctl restart nfs-server
      [  430.828865] NFSD: Using nfsdcld client tracking operations.
      [  430.837690] NFSD: no clients to reclaim, skipping NFSv4 grace period (net effffff9)
    
    The root cause is unclear, but a service restart can bypass it. This
    patch will introduce the mitigation.
    
    Signed-off-by: Tao Liu <ltao@redhat.com>

--- tests/setup/nfs_server/test.sh
+++ tests/setup/nfs_server/test.sh
@@ -10,6 +10,7 @@ rlRun "dnf -y install nfs-utils"
 rlRun "mkdir -p /var/tmp/nfsshare/var/crash"
 rlRun "echo '/var/tmp/nfsshare 192.168.0.0/16(rw,no_root_squash)' >> /etc/exports"
 rlRun "systemctl enable --now rpcbind nfs-server"
+rlRun "systemctl restart nfs-server"
 rlPhaseEnd
 
 rlJournalEnd
